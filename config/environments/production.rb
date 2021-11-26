6414d7dd77e24178c581cdedc25fc1b492a7aaca
thesaferreira <thiagofportella@gmail.com>
2021-11-26 11:57:36 -0300

    Add sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,4 +117,17 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  config.action_mailer.default_url_options = { :host => "https://thesaferreirablog.herokuapp.com" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :user_name => 'apikey',
+    :password => Rails.application.credentials.sendgrid[:api_key],
+    :address => 'smtp.sendgrid.net',
+    :domain => 'thesaferreirablog.herokuapp.com',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+
 end
