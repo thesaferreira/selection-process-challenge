46d7092fa51178a71732629572fd0be98fc4beb6
thesaferreira <thiagofportella@gmail.com>
2021-11-25 21:37:04 -0300

    :bug: Fix Foreign Key error in post taggings

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ApplicationRecord
     belongs_to :user
     has_many :comments, dependent: :destroy
 
-    has_many :taggings
+    has_many :taggings, dependent: :destroy
     has_many :tags, through: :taggings
 
     def self.tagged_with(name)
